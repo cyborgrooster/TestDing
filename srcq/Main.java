7e3e728395901596f0c947fbb116d916f02bbb14
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2019-06-25 03:21:49 +0200

    jgh

---
+++ srcq/Main.java
@@ -0,0 +1,16 @@
+package textding;
+
+//import java.util.Scanner;
+
+public class Textding {
+    
+    public static void main(String[] args) {
+        //ik weet niet wat ik aan het doen ben tbh.
+        System.out.println(">> Haai.");
+
+        while(true){ //blijft doorgaan tot dat je het zelf stopt.
+            IO.verwerk(IO.invoer()); //dit.. is idek. 
+            //ik heb goed gegokt lmao
+        }
+    }
+}
